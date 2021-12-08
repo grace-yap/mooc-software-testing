e3bdb725a0d7bb4e81f1697184490cab88394272
Grace Yap <grace.u.yap@gmail.com>
2021-12-08 12:34:16 +0800

    Practical Exercise #1: gHappy

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -4,9 +4,16 @@ public class GHappy {
 
     public boolean gHappy(String str) {
         assert str!=null;
+
+        //checks if string has no g
+        if(str.indexOf('g') < 0)
+            return false;
+
         for(int i = 0; i < str.length(); i++) {
             if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
+                //added separate checking for i = 0
+                if (i == 0 && str.charAt(i+1) == 'g') { continue; }
+                if (i > 0 && str.charAt(i-1) == 'g') { continue; }      //changed i >= 0 to i > 0
                 if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
                 return false;
             }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,45 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+     /*
+     Partitions:
+     1. null string
+     2. empty string
+     3. no g
+     4. capital G
+     5. with g
+          a. single g
+          b. multiple g's
+               - right end
+               - left end
+               - middle
+          c. combinations
+               - single g + single g
+               - single g + multiple g's
+               - multiple g's + multiple g's
+      */
+
+     @ParameterizedTest(name = "{0}: {1}, {2}")        //Description: input, expectedResult
+     @CsvSource({
+             // Null string
+             "Null string,null,false",
+             // Empty string
+             "Empty string,'',false",
+             // No g
+             "No g,asdfasdf,false",
+             // Capital G
+             "Captial G,asdfGasdf,false", "Captial G,asdfGGasdf,false",
+             // Multiple g's
+             "Multiple g's left,ggxxxx,true", "Multiple g's middle,xgggxxxx,true", "Multiple g's right,xxgggg,true",
+             // Combinations
+             "Single g + single g,gxgx,false", "Single g + multiple g,xgxxgg,false", "Multiple g + multiple g,xxggxxxxxxgggxxx,true"
+     })
+     public void testgHappy(String partition, String input, boolean expectedResult){
+          boolean result = new GHappy().gHappy(input);
+          Assertions.assertSame(expectedResult, result);
+     }
 }
